bb8ae5686d2a93187a967dba79e09c3691b54224
Jimmi Dyson <jimmidyson@gmail.com>
2017-01-13 13:49:19 +0000

    Copy circle.yml over to gh-pages branch when building docs

--- ci-docs.sh
+++ ci-docs.sh
@@ -7,7 +7,7 @@ echo "============================================"
 git clone -b gh-pages git@github.com:redhat-ipaas/ipaas-ui-style-guide.git gh-pages && \
 git config --global user.email "jimmidyson+jimmibot@gmail.com" && \
 git config --global user.name "jimmibot" && \
-cp -rv target/generated-docs/* gh-pages/ && \
+cp -rv target/generated-docs/* circle.yml gh-pages/ && \
 cd gh-pages && \
 mv index.pdf ipaas-ui-style-guide.pdf && \
 git add --ignore-errors * && \
